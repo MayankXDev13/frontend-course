f0d8b6d0323ae25c8d26874ae96f88b95832b8b5
Mayank <vijaymaynk2003@gmail.com>
2025-12-15 12:29:43 +0530

    add files to git

---
+++ 01_start/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn react with Chaicode.com</h1>
+      <p>This is a sub heading</p>
+    </>
+  );
+}
+
+export default App;
