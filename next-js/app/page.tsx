b3edac1dd0d9c0773295fd1f1ac01c53621468c7
Mayank <vijaymaynk2003@gmail.com>
2025-12-20 15:22:23 +0530

    next js start

---
+++ next-js/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <h1 className="text-4xl text-green-500 font-extrabold">Hello World</h1>
+    </div>
+  );
+}
