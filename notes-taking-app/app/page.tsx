f9b5ffe7e997dad45d0794b676f19068fe1fccd8
Mayank <vijaymaynk2003@gmail.com>
2026-01-27 17:11:42 +0530

    chore: Remove debug console log from notes page.

--- notes-taking-app/app/page.tsx
+++ notes-taking-app/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
 
   const notes = await getNotes()
 
-  console.log(notes)
+  
   return (
    <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Notes App</h1>
